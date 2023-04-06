b6a74ac4974dd92db9295ab81bf45bbe2cb4cbf4
TonyL <Tony.liang@qualicom.com.hk>
2023-04-06 11:32:10 -0400

    add stemming

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import pandas as pd
-import os
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import ConfusionMatrixDisplay
+from nltk.stem.lancaster import LancasterStemmer
+
 
 def main():
     ## 1) Load the data into a pandas data frame. ## (done)
-    data = pd.read_csv(r'Youtube01-Psy.csv')
+    data = pd.read_csv(r'Youtube03-LMFAO.csv')
 
     ## 2) .....(To Do)
 
@@ -23,28 +22,26 @@ def main():
     stop_words = set(stopwords.words("english"))
 
     # remove stop words
+    lancaster = LancasterStemmer()
     data["TOKENS"] = data["CONTENT"].apply(word_tokenize)
     data["TOKENS"] = data["TOKENS"].apply(lambda tokens: [token for token in tokens if token.lower() not in stop_words])
+    data["TOKENS"] = data["TOKENS"].apply(lambda tokens: [lancaster.stem(token) for token in tokens])
     data["TOKENS_STR"] = data["TOKENS"].apply(lambda tokens: " ".join(tokens))
 
     # create the word_count vector
     vectorizer = CountVectorizer()
-    # FEATURES = vectorizer.fit_transform(data["TOKENS_STR"]).toarray()
     init_features = vectorizer.fit_transform(data["TOKENS_STR"]).toarray()
 
     ## 4) explore initial features (not completed)
     print("Values bigger than zero:", [ele for ele in init_features[0] if ele > 0])
 
     ## 5) Downscale the transformed data ##  (final_features exploration incompleted) ##
-
     # used to find the importance of the words
     tfidf = TfidfTransformer()
     final_features= tfidf.fit_transform(init_features)
     print("Values bigger tha zero after downscaling : ",[ele for ele in final_features.toarray()[0] if ele > 0])   ## compare to line 16
 
     ## 6) shuffle the dataset (done)  ##   (completed)
-    # final_features.sample(frac = 1)
-
     # turn the final_features d-array to panada data_frame
     scaled_features_df = pd.DataFrame(final_features.toarray())
     # concat scaled_features_df and data["CLASS"] to form a new data frame
@@ -62,8 +59,7 @@ def main():
     y_test = testing_pd["CLASS"]
 
     ## 8) Fit the training data into a Naive Bayes classifier.  ##
-
-    # build a model with the multinomialNB classifier
+    # build a model with the multinomial NB classifier
     clf = MultinomialNB()
     clf.fit(X_train, y_train)
 
@@ -75,10 +71,9 @@ def main():
     ## useful for 10)
     # Compute prediction
     y_pred = clf.predict(X_test)
-    print(accuracy_score(y_test, y_pred))
-
-    cm = confusion_matrix(y_test, y_pred, labels = [0, 1])
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels = [0,1])
+    print("the accuracy_score is : ", accuracy_score(y_test, y_pred))
+    cm = confusion_matrix(y_test, y_pred, labels=[0, 1])
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[0,1])
     disp.plot()
 
 if __name__ == "__main__":
